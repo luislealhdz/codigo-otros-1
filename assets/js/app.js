b1c70e475c7d6a5e2f9fb173264274eddbe7178d
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-02-08 17:17:10 -0600

    SAC-SEC1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,40 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Luis Leal - Califación 10/5
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+const baseEndpoint = "https://api.github.com";
+const usersEndpoint = `${baseEndpoint}/users`;
+const $username = document.querySelector(".name");
+
+// Mandar a llamar a una clase y no a un id.
+const $userBlog = document.querySelector(".blog");
+
+const $userLocation = document.querySelector(".location");
+
+// Se agregó que también se mostrara la imagen
+const $avatar = document.querySelector("#avatar");
+
+// Se añadió async para esperar la respuesta de la API
+async function displayUser(username) {
+    $username.textContent = "cargando...";
+    const response = await fetch(`${usersEndpoint}/${username}`);
+
+    // Se obtiene la respuesta y la pasamos en formato json
+    data = await response.json();
+
+    // Se sustituye '' por `` al estar usando template string ${}
+    githubUser = `${data.name}`;
+    $username.textContent = githubUser;
+    $userBlog.textContent = `${data.blog}`;
+    $userLocation.textContent = `${data.location}`;
+
+    // Se agrega la información al elemento img para mostrar el avatar
+    $avatar.src = `${data.avatar_url}`;
+    $avatar.alt = githubUser;
 }
 
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+    console.log("OH NO!");
+    console.log(err);
+    n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser("stolinski").catch(handleError);
